bab7b650ac43a3f921d460e4b483876e59ee7777
vladyslavpukhalskyi <vladyslav.pukhalskyi@oa.edu.ua>
2024-11-22 12:06:28 +0200

    Initial commit

---
+++ src/apiMethods.js
@@ -0,0 +1,18 @@
+/*
+  Goto https://reqres.in/ for documentation on this api.
+  
+  If you haven't used axios, documentation here: https://github.com/axios/axios
+  OR use any method / library you're comfortable with to perform the request(s).
+
+  **** These stubs are just provided as a convienece, ****
+  **** feel free to change whatever you like to accomplish the goal. ****
+*/
+
+import axios from "axios";
+
+const baseUrl = "https://reqres.in/";
+
+export const getUsers = () => {};
+
+// Bonus:
+export const deleteUser = () => {};
